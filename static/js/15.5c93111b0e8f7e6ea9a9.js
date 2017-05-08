7d91d62ccb4c1490b0ae80b9bebe2cd5e0d25eb9
ChaselWu <minowu@foxmail.com>
2017-05-08 15:46:57 +0800

    Update to 2.4.2

--- static/js/15.5c93111b0e8f7e6ea9a9.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([15],{492:function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={mn:{cell:{allSelectedText:"全选",noDataText:"没有找到合适的记录"},popup:{confirmText:"确认",cancelText:"取消",closeText:"关闭",confirmTitle:"确认操作",alertTitle:"警告",messageText:"你有一条消息"}}}}});
--- static/js/16.6c63f90605ce998c679a.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([16],{491:function(e,o,t){"use strict";Object.defineProperty(o,"__esModule",{value:!0}),o.default={examples:{menus:{core:"核心",layout:"布局",form:"表单",components:"组件库"}}}}});
